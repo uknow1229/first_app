5a84ab1f45db9a0919459d9a7eab132207541ae9
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-03-19 04:57:26 +0000

    [Fix] 2-9

--- ruby/class.rb
+++ ruby/class.rb
@@ -2,6 +2,15 @@ array = [1,2,3]
 puts array.class
 puts array.join("*")
 
-string = "Hello"
-puts string.class
-puts string.join("*")
+# string = "Hello"
+# puts string.class
+# puts string.join("*")
+
+class Car
+  def self.turn(direction)
+      puts "#{direction}に曲がります。"
+  end
+end
+
+
+Car.turn("右")
